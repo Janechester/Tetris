fb81934e5c8bb503455507e399a35015e90e7c7c
Jake Shasteen <jakeshasteen@gmail.com>
2016-01-16 23:52:02 -0800

    Draw blocks

--- tetris.js
+++ tetris.js
@@ -33,5 +33,23 @@ Pieces
 
 Require: jQuery
 */
+var blockSize = 20;
+var canvasHeight = blockSize * 22;
+var canvasWidth = blockSize * 10;
+var $canvas = $('<canvas id="playArea" width="' + canvasWidth + '" height="' + canvasHeight + '"></canvas>')
+$canvas.appendTo( $('body') );
+var ctx = $canvas[0].getContext('2d');
 
-$('<canvas id="playArea" width="320" height="480"></canvas>').appendTo( $('body') );
+var drawBlock = function( x, y, color ) {
+  ctx.beginPath();
+  ctx.rect( ( x * blockSize), ( y * blockSize ), blockSize, blockSize ); // set rect position and size
+  ctx.fillStyle = color || '#fff'; // set fill color
+  ctx.fill(); // draw fill
+  ctx.stroke(); // draw border
+  ctx.closePath();
+};
+
+drawBlock( 3, 3 );
+drawBlock( 4, 3 );
+drawBlock( 5, 3 );
+drawBlock( 6, 3 );
